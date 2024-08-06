5ed2ab1c79c2ca2062c1c76bd95e01b502ed404c
silgenius <olutademartin@gmail.com>
2024-08-05 20:09:39 -0700

    Update

--- api/v1/views/amenities.py
+++ api/v1/views/amenities.py
@@ -32,7 +32,7 @@ def get_amenity(amenity_id):
             return jsonify(amenity.to_dict())
     abort(404)
 
-@app_views.route("amenities/<string:amenity_id>" methods=['DELETE'])
+@app_views.route("amenities/<string:amenity_id>", methods=['DELETE'])
 def delete_amenity(amenity_id):
     amenities = storage.all(Amenity).values()
     for amenity in amenities:
